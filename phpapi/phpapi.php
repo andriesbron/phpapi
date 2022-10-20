7ef402cc612fe115bd51a6b6eddb880cfcab3829
andriesbron <andries@inourplace.nl>
2022-10-20 21:21:17 +0200

    Update phpapi.php

--- phpapi/phpapi.php
+++ phpapi/phpapi.php
@@ -69,3 +69,42 @@ class math
          return (((($uCount + ($uCount >> 3)) & 030707070707) % 63) %2 == 0);
     }
 }
+
+
+class randomMath
+{
+    
+    /**
+     * Generate a random string, using a cryptographically secure
+     * pseudorandom number generator (random_int)
+     *
+     * For PHP 7, random_int is a PHP core function
+     * For PHP 5.x, depends on https://github.com/paragonie/random_compat
+     *
+     * @param int $length      How many characters do we want?
+     * @param string $keyspace A string of all possible characters
+     *                         to select from
+     * @return string
+     */
+    static public function random_str(
+        $length,
+        $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        ) 
+    {
+        $str = '';
+
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        if ($max < 1) {
+                throw new Exception('$keyspace must be at least two characters long');
+        }
+        for ($i = 0; $i < $length; ++$i) {
+                $str .= $keyspace[random_int(0, $max)];
+        }
+
+        return $str;
+    }
+}
+
+
+
+
